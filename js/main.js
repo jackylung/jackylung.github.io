2a95e5cef3eb84922cb7b95ad0d39d50f0d5b1fc
jackylung <60654446+jackylung@users.noreply.github.com>
2020-02-08 19:33:59 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,13 +3,54 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 */
 
-let myImage = document.querySelector('img');
+//let myImage = document.querySelector('img');
+let imageToChange = document.querySelector("#learning"); //#learning is the spcific id of specific image in index
 
-myImage.onclick = function(){
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/selfie.png'){
-        myImage.setAttribute('src','images/js_image.jpg')
+imageToChange.onclick = function(){
+    let mySrc = imageToChange.getAttribute('src');
+    if(mySrc === 'images/learning.png'){
+      imageToChange.setAttribute('src','images/js_image.jpg')
     } else{
-        myImage.setAttribute('src','images/selfie.png')
+      imageToChange.setAttribute('src','images/learning.png')
     }
 }
+
+//script to control multiple pages in one HTML
+/*function show(shown, hidden) {
+			  document.getElementById(shown).style.display='block';
+			  document.getElementById(hidden).style.display='none';
+			  return false;
+			}
+			*/
+function show(pageNoToShow,nonShowPage1,nonShowPage2,nonShowPage3,nonShowPage4) {
+  document.getElementById(pageNoToShow).style.display='block';
+	document.getElementById(nonShowPage1).style.display='none';
+  document.getElementById(nonShowPage2).style.display='none';
+  document.getElementById(nonShowPage3).style.display='none';
+  document.getElementById(nonShowPage4).style.display='none';
+	return false;
+}			
+
+/*
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+  
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+  }
+  */
